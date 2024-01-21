e720850f90c5964cbafc4aa2dad9b39e9bc6507d
Kirill <K-baranov@outlook.com>
2024-01-21 21:40:50 +0100

    Initial Commit

---
+++ ChallengeDay60/ChallengeDay60App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay60App.swift
+//  ChallengeDay60
+//
+//  Created by Kirill Baranov on 21/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay60App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
